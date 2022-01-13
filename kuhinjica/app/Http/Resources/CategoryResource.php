2878ce7f894fd57d34bd4ef25ac40d2a88223110
Milica Maslarevic <milicamaslarevic@gmail.com>
2022-01-13 15:23:24 +0100

    Kreirani User i Category resursi

--- kuhinjica/app/Http/Resources/CategoryResource.php
+++ kuhinjica/app/Http/Resources/CategoryResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class RecipeResource extends JsonResource
+class CategoryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -12,18 +12,12 @@ class RecipeResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-
-     public static $wrap='recipe';
     public function toArray($request)
     {
         return [
             'id'=>$this->resource->id,
-            'title'=>$this->resource->title,
-            'body'=>$this->resource->body,
-            'category'=>new CategoryResource($this->resource->category),
-         
-            'user'=>new UserResource($this->resource->user)
-
+            'name'=>$this->resource->name,
+            
 
         ];
     }
